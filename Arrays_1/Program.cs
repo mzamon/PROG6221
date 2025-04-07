647c9140c24716c35e6b36be22af2a2374df686c
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-04-07 08:12:10 +0000

    Second

--- Arrays_1/Program.cs
+++ Arrays_1/Program.cs
@@ -10,6 +10,30 @@ namespace Arrays_1
     {
         static void Main(string[] args)
         {
+            //Integer Array
+            int[] iNumbers = new int[5];
+            //String Array
+            string[] sNames = new string[5];
+
+            //Populate iNumbers Array
+            for (int i = 0; i < iNumbers.Length; i++)
+            {
+                Console.Write($"Enter a number for index value: {i} : ");
+                iNumbers[i] = int.Parse(Console.ReadLine());
+            }
+            //Populate sNames Array
+            for (int i = 0; i < sNames.Length; i++)
+            {
+                Console.Write($"Enter a name for index value: {i} : ");
+                sNames[i] = Console.ReadLine();
+            }
+            //Print iNumbers array
+            Console.WriteLine("\nThe values for number are:");
+            foreach (int Num in iNumbers)
+            {
+                Console.WriteLine(Num);
+            }
+            Console.WriteLine("\nEND OF LINE");
         }
     }
 }
